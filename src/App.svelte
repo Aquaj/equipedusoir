e8bd4316e6c03f25f326829eab0c30c809f5bdf7
Jérémie <jeremie.bonal@gmail.com>
2021-05-15 01:04:16 +0200

    Add more wiggling.

--- src/App.svelte
+++ src/App.svelte
@@ -11,6 +11,12 @@
         var old_rot = member.rot;
         member.rot = amplitude - Math.floor(Math.random() * amplitude*2);
       } while (Math.abs(old_rot - member.rot) < amplitude/2);
+      do {
+        var amplitude = 25
+        var old_pos = member.pos;
+        member.pos_x = amplitude - Math.floor(Math.random() * amplitude*2);
+        member.pos_y = amplitude - Math.floor(Math.random() * amplitude*2);
+      } while (Math.abs(old_pos - member.pos) < amplitude/2);
     })
     members = members; // NOTE: To trigger reactivity
   }, 1000);
@@ -27,7 +33,10 @@
     style='position: absolute;
       top: {member.position[0]};
       left: {member.position[1]};
-      transform: rotate({member.rot}deg);'
+      transform:
+        rotate({member.rot}deg)
+        translate({member.pos_x}px, {member.pos_y}px)
+    ;'
     >
   {/each}
   <Title content="c nous" style="bottom" />
