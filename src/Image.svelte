2f03ef832994759ab490b404709e5befa2dcdc4d
Jérémie <jeremie.bonal@gmail.com>
2021-05-15 02:20:34 +0200

    Add descriptions.

--- src/Image.svelte
+++ src/Image.svelte
@@ -4,6 +4,7 @@
   export let name;
   export let x;
   export let y;
+  export let description;
 
   let rot;
   let pos_x;
@@ -30,21 +31,46 @@
   onDestroy(() => clearInterval(interval));
 </script>
 
-<img
-  alt={name}
-  class='member'
-  src='./images/{name}.webp'
-  style='position: absolute;
-    top: {y};
-    left: {x};
-    transform:
-    rotate({rot}deg)
-    translate({pos_x}px, {pos_y}px)
+<div class='image-container' style='position: absolute; top: {y}; left: {x};'>
+  <img
+    alt={name}
+    class='member'
+    src='./images/{name}.webp'
+    style='transform:
+      rotate({rot}deg)
+      translate({pos_x}px, {pos_y}px)
     ;'
- />
+  />
+  <div class='description'>
+    {description || ''}
+  </div>
+</div>
 
 <style>
+  .image-container {
+    position: relative;
+    z-index: 10; /* Catch the hover */
+  }
+
   img.member {
     height: 250px;
+  }
+
+  .image-container:hover .description {
+    display: block;
+  }
+
+  .description {
+    display: none;
+
+    position: absolute;
+    top: 70%;
+    left: 40%;
+
+    width: 175px;
+    padding: 15px;
+
+    background-color: white;
+    border: solid black 1px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,24 @@ const app = new App({
   target: document.body,
   props: {
     members: [
-      { name: 'moon',  x: '45vw', y: '29vh' },
-      { name: 'niclo', x: '19vw', y: '50vh' },
-      { name: 'sharl', x: '67vw', y: '48vh' },
-      { name: 'amine', x: '44vw', y: '69vh' },
+      { name: 'moon',
+        x: '45vw', y: '29vh',
+        description: 'Moon "Coucou" Five: inventeur de chargeur, ' +
+          'écrivain de la pensée, raciste.',
+      }, {
+        name: 'niclo',
+        x: '19vw', y: '50vh',
+        description: "L'amie des animaux, Touhou player extraordinaire.",
+      }, {
+        name: 'sharl',
+        x: '67vw', y: '48vh',
+        description: 'sharl "Coucou" sharl: ' +
+          'possesseur de chargeur, mange tes morts Moon.',
+      }, {
+        name: 'amine',
+        x: '44vw', y: '69vh',
+        description: 'Poti blagueur en chef.',
+      },
     ],
   }
 });
